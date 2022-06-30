2e978a33ccb1ce7edada96d79d47b04bce6e4a40
cryptomehedi <classicalmehedi@gmail.com>
2022-06-30 22:41:34 +0600

    tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Nav from "./Components/Nav";
+import Tasks from "./Components/Tasks";
 
 function App() {
   return (
     <div>
       <Nav/>
+      <Routes>
+        <Route path="/" element={<Tasks/>} />
+      </Routes>
     </div>
   );
 }
